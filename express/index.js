83589050bcc49582b21bd0d1c8efb8598e34bbaa
Kevin Krato <kratok08@gmail.com>
2019-08-03 19:20:22 -0600

    Return on error

--- express/index.js
+++ express/index.js
@@ -23,16 +23,12 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find(course => course.id === parseInt(req.params.id));
   if (!course)
-    res.status(404).send("The course with the given ID was not found.");
-  res.send(course.name);
+    return res.status(404).send("The course with the given ID was not found.");
 });
 
 app.post("/api/courses", (req, res) => {
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -47,20 +43,32 @@ app.put("/api/courses/:id", (req, res) => {
     course => course.id === parseInt(req.params.id)
   );
   if (!matchingCourse)
-    res.status(404).send("The course with the given id was not found.");
+    return res.status(404).send("The course with the given id was not found.");
 
   //input validation
   // const result = validateCourse(req.body);
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   //update course
   matchingCourse.name = req.body.name;
 
   //return updated course
+  res.send(matchingCourse);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  //look up
+  const matchingCourse = courses.find(
+    course => course.id === parseInt(req.params.id)
+  );
+  //not existing, 404
+  if (!matchingCourse)
+    return res.status(404).send("The course with the given id was not found.");
+  //delete
+  const index = courses.indexOf(matchingCourse);
+  courses.splice(index, 1);
+  //return same course
   res.send(matchingCourse);
 });
 
