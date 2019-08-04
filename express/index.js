0789ab3f475c5bacb083207b49c05cd3777ac01f
Kevin Krato <kratok08@gmail.com>
2019-08-03 18:18:40 -0600

    Handle PUT and POST requests

--- express/index.js
+++ express/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Joi = require("@hapi/joi");
 const app = express();
 
 //middleware used by app
@@ -27,6 +28,11 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -35,6 +41,38 @@ app.post("/api/courses", (req, res) => {
   res.send(course);
 });
 
+app.put("/api/courses/:id", (req, res) => {
+  //locate course
+  const matchingCourse = courses.find(
+    course => course.id === parseInt(req.params.id)
+  );
+  if (!matchingCourse)
+    res.status(404).send("The course with the given id was not found.");
+
+  //input validation
+  // const result = validateCourse(req.body);
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  //update course
+  matchingCourse.name = req.body.name;
+
+  //return updated course
+  res.send(matchingCourse);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}...`));
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+  return Joi.validate(course, schema);
+}
